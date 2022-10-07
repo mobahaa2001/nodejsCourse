33562b8ccdba238b2a157439d75e8e65786df438
Guide <70282836+mobahaa2001@users.noreply.github.com>
2022-10-07 18:42:51 +0200

    Update app.js

--- app.js
+++ app.js
@@ -27,3 +27,22 @@ fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
   });
 });
 console.log("Will read file!");
+
+//////////////////////////////////////
+/* Creating a simple web server */
+/////////////////////////////////////
+
+const http = require("http");
+///////////////////////////
+// Files
+
+/////////////////////////
+// Server
+const server = http.createServer((req, res) => {
+  // console.log(req);
+  res.end("Hello from the mo server!");
+});
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Listening to requests on port 8000");
+});
