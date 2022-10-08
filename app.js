117340ce0c0db6afa081a9ee1559b399caf3a37b
Guide <70282836+mobahaa2001@users.noreply.github.com>
2022-10-08 03:24:58 +0200

    Update app.js

--- app.js
+++ app.js
@@ -46,3 +46,35 @@ const server = http.createServer((req, res) => {
 server.listen(8000, "127.0.0.1", () => {
   console.log("Listening to requests on port 8000");
 });
+////////////////////////////
+/*Creating a simple web server & Routing */
+///////////////////////////
+const http = require("http");
+const url = require("url");
+
+///////////////////////////
+// Files
+
+/////////////////////////
+// Server
+const server = http.createServer((req, res) => {
+  // console.log(req.url);
+  const pathName = req.url;
+
+  if (pathName === "/" || pathName === "/overview") {
+    res.end("This is OVERVIEW");
+  } else if (pathName === "/product") {
+    res.end("This is PRODUCT");
+  } else {
+    res.writeHead(404, {
+      "Content-type": "text/html",
+      "my-own-header": "hello-world",
+    });
+    res.end("<h1>Page not found!</h1>");
+  }
+});
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Listening to requests on port 8000");
+});
+
