a76d0434c8ca9c6711f07edc86ba48034fe37c95
Guide <70282836+mobahaa2001@users.noreply.github.com>
2022-10-09 02:04:27 +0200

    Update app.js

--- app.js
+++ app.js
@@ -78,3 +78,45 @@ server.listen(8000, "127.0.0.1", () => {
   console.log("Listening to requests on port 8000");
 });
 
+/////////////////////////////////////////
+/* Building a (Very) Simple API */
+////////////////////////////////////////
+
+const http = require("http");
+
+////////////////////////////
+/* Routing */
+///////////////////////////
+const url = require("url");
+
+///////////////////////////
+// Files
+
+/////////////////////////
+// Server
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const dataObj = JSON.parse(data);
+
+const server = http.createServer((req, res) => {
+  // console.log(req.url);
+  const pathName = req.url;
+
+  if (pathName === "/" || pathName === "/overview") {
+    res.end("This is OVERVIEW");
+  } else if (pathName === "/product") {
+    res.end("This is PRODUCT");
+  } else if (pathName === "/api") {
+    res.writeHead(200, {"Content-type": "application/json"});
+    res.end(data);
+  } else {
+    res.writeHead(404, {
+      "Content-type": "text/html",
+      "my-own-header": "hello-world",
+    });
+    res.end("<h1>Page not found!</h1>");
+  }
+});
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Listening to requests on port 8000");
+});
