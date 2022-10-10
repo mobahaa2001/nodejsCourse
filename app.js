9662ad1f18264504fdff73ee844039a6b0a64754
Guide <70282836+mobahaa2001@users.noreply.github.com>
2022-10-10 06:13:51 +0200

    Update app.js

--- app.js
+++ app.js
@@ -293,3 +293,86 @@ const server = http.createServer((req, res) => {
 server.listen(8000, "127.0.0.1", () => {
   console.log("Listening to requests on port 8000");
 });
+///////////////////////////////////////
+/* New Modules */
+///////////////////////////////////////
+const fs = require("fs");
+const slugify = require("slugify");
+const replaceTemplate = require("./modules/replaceTemplate");
+//////////////////////////////////////
+/* Creating a simple web server */
+/////////////////////////////////////
+
+const http = require("http");
+
+////////////////////////////
+/* Routing */
+///////////////////////////
+const url = require("url");
+
+///////////////////////////
+// Files
+
+/////////////////////////
+// Server
+
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  "utf-8"
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  "utf-8"
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  "utf-8"
+);
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+
+const dataObj = JSON.parse(data);
+
+const slugs = dataObj.map((el) => slugify(el.productName, {lower: true}));
+console.log(slugs);
+
+const server = http.createServer((req, res) => {
+  const {query, pathname} = url.parse(req.url, true);
+
+  // Overview page
+  if (pathname === "/" || pathname === "/overview") {
+    res.writeHead(200, {
+      "Content-type": "text/html",
+    });
+
+    const cardsHtml = dataObj
+      .map((el) => replaceTemplate(tempCard, el))
+      .join("");
+    const output = tempOverview.replace("{%PRODUCT_CARDS%}", cardsHtml);
+    res.end(output);
+
+    // Product page
+  } else if (pathname === "/product") {
+    res.writeHead(200, {
+      "Content-type": "text/html",
+    });
+    const product = dataObj[query.id];
+    const output = replaceTemplate(tempProduct, product);
+    res.end(output);
+
+    // Api
+  } else if (pathname === "/api") {
+    res.writeHead(200, {"Content-type": "application/json"});
+    res.end(data);
+    // Not found
+  } else {
+    res.writeHead(404, {
+      "Content-type": "text/html",
+      "my-own-header": "hello-world",
+    });
+    res.end("<h1>Page not found!</h1>");
+  }
+});
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Listening to requests on port 8000");
+});
